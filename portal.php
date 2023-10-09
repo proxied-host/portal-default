a2d00066c718a1dd46a43917db74a1b5fe7900c4
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-10-09 14:46:14 +0300

    Update portal.php

--- portal.php
+++ portal.php
@@ -5,7 +5,7 @@ return [
     'name' => 'Default',
     'image' => 'https://gyazo.com/50beae3a0fd577ff976abae7066d0af9.png',
     'version' => '1.0.0',
-    'wemx_version' => '1.0.0', 
+    'wemx_version' => '1.7.1',
     'author' => 'Mubeen',
 
     // Below are assets for the theme
