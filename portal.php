438fc8d708918fd920f7c705fef6c6cb48082a29
djoh <djoh@proxied.host>
2024-02-05 20:51:26 +0200

    update theme name and author

--- portal.php
+++ portal.php
@@ -2,14 +2,14 @@
 
 return [
 
-    'name' => 'Default',
+    'name' => 'proxied.host',
     'image' => 'https://gyazo.com/50beae3a0fd577ff976abae7066d0af9.png',
     'version' => '1.0.0',
     'wemx_version' => '1.7.1',
-    'author' => 'Mubeen',
+    'author' => 'Mubeen & djoh',
 
     // Below are assets for the theme
-    'folder' => 'default',
+    'folder' => 'portal-custom',
     'assets' => '/assets/themes/default/'
 
 ];
