905b4ca310c69cb3de1c7223e15da067dff865b6
djoh <djoh@proxied.host>
2024-02-13 20:31:25 +0200

    update name

--- portal.php
+++ portal.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'name' => 'proxied.host',
+    'name' => 'portal-custom',
     'image' => 'https://gyazo.com/50beae3a0fd577ff976abae7066d0af9.png',
     'version' => '1.0.0',
     'wemx_version' => '1.7.1',
